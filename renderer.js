ed2bd1eb08cb21135250bbc34a21c574a53c03cc
York Xiang <bombless@126.com>
2022-06-28 18:39:35 +0800

    离线查询城市经纬度

--- renderer.js
+++ renderer.js
@@ -6,15 +6,15 @@
 // process.
 
     AMapLoader.load({
-        "key": "bd025ef2a45752cd896864d70447a76f",              // 申请好的Web端开发者Key，首次调用 load 时必填
+        "key": "bd025ef2a45752cd896864d70447a76f",          // 申请好的Web端开发者Key，首次调用 load 时必填
         "version": "2.0",   // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
         "plugins": ['AMap.Geocoder', 'AMap.Weather'],           // 需要使用的的插件列表，如比例尺'AMap.Scale'等
-        "AMapUI": {             // 是否加载 AMapUI，缺省不加载
-            "version": '1.1',   // AMapUI 版本
-            "plugins":['overlay/SimpleMarker'],       // 需要加载的 AMapUI ui插件
+        "AMapUI": {         // 是否加载 AMapUI，缺省不加载
+        "version": '1.1',   // AMapUI 版本
+        "plugins":['overlay/SimpleMarker'],       // 需要加载的 AMapUI ui插件
         },
-        "Loca":{            // 是否加载 Loca， 缺省不加载
-            "version": '2.0'  // Loca 版本
+        "Loca":{        // 是否加载 Loca， 缺省不加载
+        "version": '2.0'  // Loca 版本
         },
     }).then((AMap)=>{
         const map = new AMap.Map('container');
@@ -23,9 +23,49 @@
     }).catch((e)=>{
         console.error(e);  //加载错误提示
     });
+    function regions() {
+        const geolocation = require('./region.json');
+        const data = {};
+        for (const province of geolocation.districts) {
+            const entry = province.name;
+            for (const city of province.districts) {
+                const key = city.name;
+                if (!data[entry]) {
+                    data[entry] = {};
+                }
+                data[entry][key] = [city.center.longitude, city.center.latitude];
+            }
+        }
+
+        return (province, city) => {
+        for (const entry of Object.keys(data)) {
+            if (entry.indexOf(province) > -1) {
+                for (const key of Object.keys(data[entry])) {
+                    if (key.indexOf(city) > -1) {
+                        return data[entry][key];
+                    }
+                }
+            }
+        }
+        }
+
+    }
+    function mark(map, info, fit) {
+        const getPosition = regions();
+        const location = getPosition(info.province, info.city);
+        var marker = new AMap.Marker({
+            icon: 'http://vdata.amap.com/icons/b18/1/2.png',
+            position: location,   // 经纬度对象，也可以是经纬度构成的一维数组[116.39, 39.9]
+            title: info.province + info.city,
+        });
+        
+        map.setFitView();
+
+        // 将创建的点标记添加到已有的地图实例：
+        map.add(marker);
+        return marker;        
+    }
     async function init(AMap, map) {
-        const weather = new AMap.Weather();
-        let markerCount = 0;
         const fetchData = require('./fetch.ts');
         const tabs = document.querySelector('#tabs');
         const data = await fetchData();
@@ -38,17 +78,17 @@
         cityOl.className = 'pannel';
         provinces.set('全国', { dom: cityOl, cities })
         tabs.addEventListener('click', e => {
-            pannel.querySelectorAll('.pannel').forEach(pannel => {
-                // console.log(e.target.textContent, pannel.dataset.name);
-                map.setFitView(provinces.get(e.target.textContent).cities);
-                if (e.target.textContent === pannel.dataset.name) {
-                    pannel.style.display = 'block';
-                }
-                else {
-                    pannel.style.display = 'none';
-                }
-            })
-            // console.log(e.target);
+        pannel.querySelectorAll('.pannel').forEach(pannel => {
+            // console.log(e.target.textContent, pannel.dataset.name);
+            map.setFitView(provinces.get(e.target.textContent).cities);
+            if (e.target.textContent === pannel.dataset.name) {
+                pannel.style.display = 'block';
+            }
+            else {
+                pannel.style.display = 'none';
+            }
+        })
+        // console.log(e.target);
         })
         for (const item of data) {
             let province;
@@ -74,48 +114,30 @@
             provinceLi.textContent = item.average + ' ' + item.province + item.city;
             province.dom.appendChild(provinceLi);
             provinceLi.title = JSON.stringify(item);
-            var geocoder = new AMap.Geocoder({
-              // city 指定进行编码查询的城市，支持传入城市名、adcode 和 citycode
-              city: item.province + item.city
-            })
-              
-            geocoder.getLocation(item.province + '省' + item.city + '政府', function(status, result) {
-              if (status === 'complete' && result.info === 'OK') {
-                //console.log(result);
-                //console.log(result.geocodes[0].location.pos);
-                var marker = new AMap.Marker({
-                    icon: 'http://vdata.amap.com/icons/b18/1/2.png',
-                    position: result.geocodes[0].location,   // 经纬度对象，也可以是经纬度构成的一维数组[116.39, 39.9]
-                    title: item.province + item.city,
+            const marker = mark(map, item);
+            province.cities.push(marker);
+            cities.push(marker);
+
+            const info = document.createElement('div');
+            const cityInfo = document.createElement('div');
+            cityInfo.textContent = item.city;
+            const temperatureInfo = document.createElement('div');
+            temperatureInfo.textContent = item.range;
+            info.append(cityInfo, temperatureInfo);
+        
+            function zoom() {
+    
+                const infoWindow = new AMap.InfoWindow({
+                    content: info  //传入 dom 对象，或者 html 字符串
                 });
-                //console.log(item.province + item.city, marker);
                 
-                // 将创建的点标记添加到已有的地图实例：
-                map.add(marker);
-                province.cities.push(marker);
-                cities.push(marker);
-                markerCount += 1;
-                if (markerCount === data.length) {
-                    map.setFitView();
-                }
-                function zoom() {
-                    //map.setZoomAndCenter(11, result.geocodes[0].location);
-
-                    const infoWindow = new AMap.InfoWindow({
-                        content: item.city + '<br/>' + item.range  //传入 dom 对象，或者 html 字符串
-                    });
-                    
-                     // 打开信息窗体
-                    infoWindow.open(map, result.geocodes[0].location);
-                    map.setFitView(provinces.get(item.province).cities);
-                }
-                cityLi.addEventListener('click', zoom);
-                provinceLi.addEventListener('click', zoom);
-                //map.setZoomAndCenter(11, result.geocodes[0].location);
-
-              }
-            });
-            //break
+                 // 打开信息窗体
+                console.log(marker);
+                infoWindow.open(map, marker._position);
+                map.setFitView(provinces.get(item.province).cities);
+            }
+            cityLi.addEventListener('click', zoom);
+            provinceLi.addEventListener('click', zoom);
         }
         pannel.appendChild(cityOl);
     }
