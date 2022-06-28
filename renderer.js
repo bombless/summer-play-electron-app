4f1dc12af9900d19a5788f60890fcb23a6f407d6
York Xiang <bombless@126.com>
2022-06-28 13:53:05 +0800

    提前载入地理解码模块

--- renderer.js
+++ renderer.js
@@ -8,12 +8,12 @@
     AMapLoader.load({
         "key": "bd025ef2a45752cd896864d70447a76f",              // 申请好的Web端开发者Key，首次调用 load 时必填
         "version": "2.0",   // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
-        "plugins": [],           // 需要使用的的插件列表，如比例尺'AMap.Scale'等
+        "plugins": ['AMap.Geocoder', 'AMap.Weather'],           // 需要使用的的插件列表，如比例尺'AMap.Scale'等
         "AMapUI": {             // 是否加载 AMapUI，缺省不加载
             "version": '1.1',   // AMapUI 版本
             "plugins":['overlay/SimpleMarker'],       // 需要加载的 AMapUI ui插件
         },
-        "Loca":{                // 是否加载 Loca， 缺省不加载
+        "Loca":{            // 是否加载 Loca， 缺省不加载
             "version": '2.0'  // Loca 版本
         },
     }).then((AMap)=>{
@@ -73,41 +73,39 @@
             provinceLi.textContent = item.average + ' ' + item.province + item.city;
             province.dom.appendChild(provinceLi);
             provinceLi.title = JSON.stringify(item);
-            AMap.plugin('AMap.Geocoder', function() {
-                var geocoder = new AMap.Geocoder({
-                  // city 指定进行编码查询的城市，支持传入城市名、adcode 和 citycode
-                  city: item.province + item.city
-                })
+            var geocoder = new AMap.Geocoder({
+              // city 指定进行编码查询的城市，支持传入城市名、adcode 和 citycode
+              city: item.province + item.city
+            })
               
-                geocoder.getLocation(item.province + '省' + item.city + '政府', function(status, result) {
-                  if (status === 'complete' && result.info === 'OK') {
-                    //console.log(result);
-                    //console.log(result.geocodes[0].location.pos);
-                    var marker = new AMap.Marker({
-                        icon: 'http://vdata.amap.com/icons/b18/1/2.png',
-                        position: result.geocodes[0].location,   // 经纬度对象，也可以是经纬度构成的一维数组[116.39, 39.9]
-                        title: item.province + item.city,
-                    });
-                    //console.log(item.province + item.city, marker);
-                    
-                    // 将创建的点标记添加到已有的地图实例：
-                    map.add(marker);
-                    province.cities.push(marker);
-                    cities.push(marker);
-                    markerCount += 1;
-                    if (markerCount === data.length) {
-                        map.setFitView();
-                    }
-                    function zoom() {
-                        //map.setZoomAndCenter(11, result.geocodes[0].location);
-                        map.setFitView(provinces.get(item.province).cities);
-                    }
-                    cityLi.addEventListener('click', zoom);
-                    provinceLi.addEventListener('click', zoom);
+            geocoder.getLocation(item.province + '省' + item.city + '政府', function(status, result) {
+              if (status === 'complete' && result.info === 'OK') {
+                //console.log(result);
+                //console.log(result.geocodes[0].location.pos);
+                var marker = new AMap.Marker({
+                    icon: 'http://vdata.amap.com/icons/b18/1/2.png',
+                    position: result.geocodes[0].location,   // 经纬度对象，也可以是经纬度构成的一维数组[116.39, 39.9]
+                    title: item.province + item.city,
+                });
+                //console.log(item.province + item.city, marker);
+                
+                // 将创建的点标记添加到已有的地图实例：
+                map.add(marker);
+                province.cities.push(marker);
+                cities.push(marker);
+                markerCount += 1;
+                if (markerCount === data.length) {
+                    map.setFitView();
+                }
+                function zoom() {
                     //map.setZoomAndCenter(11, result.geocodes[0].location);
+                    map.setFitView(provinces.get(item.province).cities);
+                }
+                cityLi.addEventListener('click', zoom);
+                provinceLi.addEventListener('click', zoom);
+                //map.setZoomAndCenter(11, result.geocodes[0].location);
 
-                  }
-                })
+              }
             });
             //break
         }
